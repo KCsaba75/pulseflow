53cb36adcbcb5eca423ca492b2ed02b5ffd9e889
Kiss Csaba <kiss.csaba@scada.hu>
2026-06-27 09:38:39 +0200

    Wire up real RevenueCat Android API key

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -5,7 +5,7 @@ class AppConfig {
   static const supabaseAnonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmY3VvYmxwZXZ1anFhbnlwZ2VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI1MjIxMDUsImV4cCI6MjA5ODA5ODEwNX0.HImMC7hCpBxcvGeagdQIfvLrVN5iDOikZMojZpz4liQ';
 
-  static const revenueCatAndroidApiKey = 'YOUR-REVENUECAT-ANDROID-KEY';
+  static const revenueCatAndroidApiKey = 'goog_EeQrMKcSTeVhShbRbTugONbiwEo';
   static const revenueCatIosApiKey = 'YOUR-REVENUECAT-IOS-KEY';
 
   static const premiumEntitlementId = 'premium';
